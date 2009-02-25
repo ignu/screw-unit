1a13ca3eb7548754db9cce8c17487651b25243b8
len smith <len.smith@beyondinteraction.com>
2009-02-24 19:24:30 -0500

    modified behavior of have_been_called

--- spec/mock_spec.js
+++ spec/mock_spec.js
@@ -1,45 +1,34 @@
 Screw.Unit(function() {
-    describe("Mock",function(){
+    describe("Screw.Mock",function(){
 
         describe("When mocking an object", function(){
             var person = {};
             Screw.Mock(person, "die");
+            Screw.Mock(person, "fly");
 
-            person.die();
             it("can verify a function was called", function(){
-                expect(person).to(have_called, "die");
+                person.die();
+                expect(person.die).to(have_been_called);
             });
 
             it("can verify a function was not called", function() {
-                Screw.Mock(person, "fly");
-                expect(person).to_not(have_called, "fly");
-            });
-
-            it("can verify a function was not called", function() {
+                expect(person.fly).to_not(have_been_called);
                 person.fly();
-                expect(person).to_not(have_called, "fly");
-            });
-        });
-
-        describe("When mocking an object with parameters", function() {
-            var internet_explorer = {};
-            Screw.Mock(internet_explorer, "suck").expecting([,"hard"]);
-
-            it("can verify correct parameters were passed in", function() {
-
-            });
-        });
-
-        describe("If a mock will replace an existing function", function(){
-            var pregnant = {expecting:function() { alert('...');}};
-            var threw_an_error = false;
-
-            it("will throw an error", function() {
                 expect(function() {
-                    Screw.Mock(pregnant, "GiveBirth");                    
+                    expect(person.fly).to_not(have_been_called);
                 }).to(throw_exception);
             });
         });
 
+//        describe("When mocking an object with parameters", function() {
+//            var internet_explorer = {};
+//            //Screw.Mock(internet_explorer, "suck").expecting([,"hard"]);
+//
+//            it("can verify correct parameters were passed in", function() {
+//
+//            });
+//        });
+
+     
     });
 });
